5dd66d05fa29aaf5ca29483c6ac81503860cf7ae
Aaron Rumery <bloodhawk@users.noreply.github.com>
2014-09-29 15:31:19 -0600

    made basic routing app

--- js/teams/teamCtrl.js
+++ js/teams/teamCtrl.js
@@ -1,5 +1,35 @@
 var app = angular.module('nbaRoutes');
 
-app.controller('teamCtrl', function($scope, $routeParams, teamService){
-
+app.controller('teamCtrl', function($scope, $routeParams, teamService, teamData){
+	$scope.teamData = teamData;
+	$scope.newGame = {};
+	$scope.showNewGameForm = false;
+	$scope.toggleNewGameForm = function(){
+		$scope.showNewGameForm = !($scope.showNewGameForm);
+	};
+	if($routeParams.team === 'utahjazz') {
+		$scope.hometeam = 'Utah Jazz';
+		$scope.logoPath = 'images/jazz-logo.png'
+	}
+	if($routeParams.team === 'losangeleslakers') {
+		$scope.hometeam = 'Los Angeles Lakers';
+		$scope.logoPath = 'images/lakers-logo.png'
+	}
+	if($routeParams.team === 'miamiheat') {
+		$scope.hometeam = 'Miami Heat';
+		$scope.logoPath = 'images/heat-logo.png'
+	}
+	$scope.submitGame = function(){
+		$scope.newGame.homeTeam = $routeParams.team;
+		$scope.newGame.opponent = $scope.opponent;
+		$scope.newGame.homeTeamScore = $scope.homeScore;
+		$scope.newGame.opponentScore = $scope.opponentScore;
+		teamService.addNewGame($scope.newGame).then(function(){
+			teamService.getTeamData($scope.newGame.homeTeam).then(function(data){
+				$scope.teamData = data;
+				$scope.newGame = {};
+				$scope.showNewGameForm = false;
+			});
+		});
+	};
 });
--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -1,5 +1,36 @@
 var app = angular.module('nbaRoutes');
 
 app.service('teamService', function($http, $q){
-
+	
+	this.addNewGame = function(gameObj){
+		var url = 'https://api.parse.com/1/classes/' + gameObj.homeTeam;
+		if(parseInt(gameObj.homeTeamScore) > parseInt(gameObj.opponentScore)) {
+			gameObj.won = true;
+		} else {
+			gameObj.won = false;
+		}
+		return $http.post(url, gameObj);
+	};
+	
+	this.getTeamData = function(team){
+		var defer = $q.defer();
+		var url = 'https://api.parse.com/1/classes/' + team;
+		$http.get(url).then(function(data){
+			console.log(data);
+			var results = data.data.results;
+			var wins = 0;
+			var losses = 0;
+			for (var i = 0; i < results.length; i++) {
+				if(results[i].won){
+					wins+=1;
+				}else{
+					losses+=1;
+				}
+			}
+			results.wins = wins;
+			results.losses = losses;
+			defer.resolve(results);
+		});
+		return defer.promise;
+	};
 });
